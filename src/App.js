211c00dffb83d739c75938485860247a0bd7f274
Buchanan <Tyra.Buchanan@us.sogeti.com>
2020-01-29 14:26:35 -0500

    adding App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3005/getSogetiEmployees')
+      .then(employees => employees.json())
+      .then(_employees => {
+        console.log(_employees)
+        this.setState({
+          employees: _employees
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className= "AppContainer">
+      <div className="App">
+        <h1>Sogeti Employee Directory</h1>
+        <div className= "row">
+        {this.state.employees.map((employee, index) => {
+          return (
+          
+            <div className="media col-12 mb-4 directory-entry p-6" key={index}>
+              <img src={employee.image}
+                 className="mr-10 directory-image" alt="..." />
+              <div className="media-body">
+                <h5 className="mt-2">{employee.name}</h5>
+                <div>
+                {employee.from}
+                </div>
+                {employee.id}
+              </div>
+            </div>
+          )
+        })
+        }
+      </div>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
